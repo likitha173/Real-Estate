9d88a7e544417072915c785e7e73e6a5cec3f97a
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-23 22:56:17 +0530

    Header Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'; //6.9k (gzipped: 2.7k)
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>Header</div>
+        <section className='h-wrapper'>
+            <div className='flexCenter padding innerWidth h-container'>
+                <img src="./logo.png" alt="logo" width={100} />
+
+                <div className='flexCenter h-menu'>
+                    <a href="">Residencies</a>
+                    <a href="">Contact Us</a>
+                    <a href="">Our Value</a>
+                    <a href="">Get Started</a>
+                    <button className='button'>
+                    <a href="">Contact</a>
+                    </button>
+                </div>
+            </div>
+        </section>
     )
 }
 
