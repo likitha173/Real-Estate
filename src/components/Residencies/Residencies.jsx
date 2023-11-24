e2d747042a1e6ea882cb1df1ed165c0e18a49739
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-24 15:38:02 +0530

    Companies Done

---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+// import "swiper/css"
+// import "./Residencies.css"
+// import data from '../../utils/slider.json'
+
+// const Residencies = () => {
+//     return (
+//         <section className='r-wrapper'>
+//             Residw
+//         </section>
+//     );
+// };
+
+// export default Residencies;
